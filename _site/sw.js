fed9b59a92939c71fc3e26f60eb429df8d5b6e83
shutong at mac <shutongcs@gmail.com>
2022-11-15 18:46:34 +0800

    update the content

--- _site/sw.js
+++ _site/sw.js
@@ -1,8 +1,8 @@
-const version = '20221115181106';
+const version = '20221115184516';
 const cacheName = `static::${version}`;
 
 const buildContentBlob = () => {
-  return ["/general/2018/08/22/hello-world/","/about/","/categories/","/blog/","/","/manifest.json","/assets/search.json","/assets/styles.css","/redirects.json","/sitemap.xml","/robots.txt","/feed.xml","https://www.gravatar.com/avatar/6c0377abcf4da91cdd35dea4554b2a4c?s=52", "/assets/default-offline-image.png", "/assets/scripts/fetch.js"
+  return ["/general/2018/08/22/hello-world/","/blog/","/","/publications/","/manifest.json","/assets/search.json","/assets/styles.css","/redirects.json","/sitemap.xml","/robots.txt","/feed.xml","/logo.png", "/assets/default-offline-image.png", "/assets/scripts/fetch.js"
   ]
 }
 
--- index.md
+++ index.md
@@ -1,8 +1,5 @@
 ---
-title: Welcome to my website
-feature_image: "https://picsum.photos/1300/400?image=989"
-feature_text: |
-  ## Hello world
+title: About
 ---
 
-There isn't much going on here yet, but watch this space
+Hi! I am a Ph.D. student advised by Prof. Fangming Liu at the School of Computer of Science and Technology, Huazhong University of Science and Technology, China. My research interests include edge computing and green computing. I received my B.Sc. degree from the School of Mathematics, Hunan University, China. During 2019 - 2020, I was a visiting Ph.D. student at the HPDC group, the Department of Computer Science, Vrije Universiteit Amsterdam, The Netherlands.
